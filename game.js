3821f8ac5a5b802fb013b8087919d826673b680a
root <root@debian.debian>
2015-10-06 20:53:59 +0300

    отрисовка крестиков и ноликов

--- game.js
+++ game.js
@@ -20,8 +20,9 @@ function field (width,height)
 
     this.arrayElement = gridArray;
 
-    this.draw = function ()
+    this.draw = function (x)
     {
+        this.x = x;
         for (i = 0; i < 3; i++)
         {
             for (j = 0; j < 3; j++)
@@ -33,21 +34,40 @@ function field (width,height)
                 }
                 else if(this.arrayElement[i][j] == 1)
                 {
-                    context.fillStyle = "#FFF";
-                    context.fillRect(i * (this.width + this.padding), j * (this.height + this.padding), this.width, this.height);
+                    //this.drawCross();
                 }
             }
         }
     }
 
-    this.drawCross = function (row,col)
+    this.drawCross = function (row,col,x,y)
     {
-        //
+        this.row = row;
+        this.col = col;
+        this.x = x;
+        this.y = y;
+
+        if (this.arrayElement[this.row][this.col] == undefined)
+        {
+            context.fillStyle = "#FFF";
+            context.font = "100px Arial";
+            context.fillText("1",this.x,this.y);
+        }
     }
 
-    this.drawZero = function (argument)
+    this.drawZero = function (row,col,x,y)
     {
-        // body...
+        this.row = row;
+        this.col = col;
+        this.x = x;
+        this.y = y;
+
+        if (this.arrayElement[this.row][this.col] == undefined)
+        {
+            context.fillStyle = "#FFF";
+            context.font = "100px Arial";
+            context.fillText("0",this.x,this.y);
+        }
     }
 }
 
@@ -59,9 +79,25 @@ function userAction ()
         col = Math.floor(event.pageY / (field.width + field.padding));
         //Определяем в какую ячейку нажал пользователь.
 
-        field.arrayElement[row][col] = 1;
-        field.draw();
-        //Рисуем крестик
+        if (currentStep == 1)
+        {
+            field.drawCross(row,col,event.pageX,event.pageY);
+            //Рисуем крестик
+            field.arrayElement[row][col] = 1;
+            //Устанавлием значение элемента массива (rpw,col) = 1
+            //Если элемент массива = 1, то значит он уже занят.
+            currentStep = 0;
+        }
+        else
+        {
+            field.drawZero(row,col,event.pageX,event.pageY);
+            //Рисуем крестик
+            field.arrayElement[row][col] = 0;
+            //Устанавлием значение элемента массива (rpw,col) = 1
+            //Если элемент массива = 1, то значит он уже занят.
+            currentStep = 1;
+        }
+
     }
 }
 
@@ -71,6 +107,9 @@ function init()
     canvas.width = 480;
     canvas.height = 480;
     context = canvas.getContext("2d");
+
+    currentStep = 1;
+    //Первый ход
     
     //Рисуем игровое поле и сетку
     field = new field(150,150);
