c41c4f71d9dfd36ff9db376d1fc30910ee9560c4
jadewizard <jadewizard@yandex.ru>
2015-10-07 09:14:41 +0300

    проверка столбцов, строк и диагоналей

--- game.js
+++ game.js
@@ -108,17 +108,34 @@ function userAction ()
 
 function winCheck ()
 {
-    if((field.arrayElement[0][0] == 1) && 
-       (field.arrayElement[1][0] == 1) &&
-       (field.arrayElement[2][0] == 1))
+    for(i = 0; i < 3; i++)
     {
-        alert("win");
-    }
-    else if((field.arrayElement[0][1] == 1) && 
-            (field.arrayElement[1][1] == 1) &&
-            (field.arrayElement[2][1] == 1))
-    {
-        alert("win");
+        if (field.arrayElement[0][i] != undefined &&
+            field.arrayElement[0][i] == field.arrayElement[1][i] &&
+            field.arrayElement[0][i] == field.arrayElement[2][i])
+            alert();
+        //Проверка строк
+
+        if (field.arrayElement[i][0] != undefined &&
+            field.arrayElement[i][0] == field.arrayElement[i][1] &&
+            field.arrayElement[i][0] == field.arrayElement[i][2])
+            alert();
+        //Поверка столбцов
+
+        if (field.arrayElement[0][0] != undefined &&
+            field.arrayElement[1][1] != undefined &&
+            field.arrayElement[2][2] != undefined &&
+            field.arrayElement[0][0] == field.arrayElement[1][1] &&
+            field.arrayElement[1][1] == field.arrayElement[2][2])
+            alert();
+
+        if (field.arrayElement[0][2] != undefined &&
+            field.arrayElement[1][1] != undefined &&
+            field.arrayElement[2][0] != undefined &&
+            field.arrayElement[0][2] == field.arrayElement[1][1] &&
+            field.arrayElement[1][1] == field.arrayElement[2][0])
+            alert();
+        //Проверка диагоналей
     }
 }
 
